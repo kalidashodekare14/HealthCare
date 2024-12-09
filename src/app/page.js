a1ba1361e1aeeb3e938d6653abba626e50699605
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-12-09 13:38:10 +0600

    HealthCare Project Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Banner from "@/components/Banner";
 import BookNow from "@/components/BookNow";
 import Image from "next/image";
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Banner></Banner>
       <BookNow></BookNow>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+import image from '../../public/image/about_doc.jpg'
+import image1 from '../../public/image/about_doc1.jpg'
+import image2 from '../../public/image/about_doc2.jpg'
+
+const About = () => {
+    return (
+        <div className='mx-32 my-5 flex items-center justify-between'>
+            <div className='relative w-[40%]'>
+                <Image className='w-full' src={image} width={500} height={300}></Image>
+                <div className='flex w-[50%]'>
+                    <Image className='w-full' src={image1} width={500} height={300}></Image>
+                    <Image className='w-full' src={image2} width={500} height={300}></Image>
+                </div>
+                <div className='absolute top-1/2 left-[35%] bg-[#307bc4]  text-white w-32 h-32 rounded-full flex flex-col justify-center items-center'>
+                    <h1 className='text-4xl'>26+</h1>
+                    <p>Experience</p>
+                </div>
+            </div>
+            <div className='w-[50%] space-y-5'>
+                <span className='text-[#307bc4] uppercase font-bold'>Our About Us</span>
+                <h1 className='text-4xl font-bold'>More Than 26+ Years About Provide Medical.</h1>
+                <p>We are privileged to work with hundreds of future-thinking medial, including many of the world’s top hardware, software, and brands , feel safe and comfortable in establishing.</p>
+                <button className='btn bg-[#307bc4] text-white'>About More</button>
+            </div>
+        </div>
+    )
+}
+
+export default About
