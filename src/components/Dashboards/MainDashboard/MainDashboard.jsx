c4360206f1e066aa7608a1eae96e1d1634d1774b
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-01-07 11:37:55 +0600

    Dashboard Route Chart Implement

--- src/components/Dashboards/MainDashboard/MainDashboard.jsx
+++ src/components/Dashboards/MainDashboard/MainDashboard.jsx
@@ -3,6 +3,98 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React from 'react'
 import { FaDollarSign, FaHeartPulse, FaStethoscope, FaWpforms } from 'react-icons/fa6'
+import { AreaChart, BarChart, Bar, Legend, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+const data2 = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 
 
 const MainDashboard = () => {
@@ -71,6 +163,62 @@ const MainDashboard = () => {
                     <div className='w-10 h-10 rounded-full flex justify-center items-center border'>
                         <FaDollarSign />
                     </div>
+                </div>
+            </div>
+            <div className='flex items-center gap-3 my-10 border'>
+                <div className='w-[600px] h-[400px] bg-white font-rubik p-5'>
+                    <div className='flex justify-between items-center text-[18px] my-3'>
+                        <h1>Revenue</h1>
+                        <h1>$4521</h1>
+                    </div>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart
+                            className=''
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 20,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+                            <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+                <div className='w-full h-[400px] bg-white p-5 font-rubik'>
+                    <div className='text-[18px] my-5'>
+                        <h1>Patient Statistic</h1>
+                    </div>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart
+                            width={500}
+                            height={400}
+                            data={data}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                            <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                            <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                        </AreaChart>
+                    </ResponsiveContainer>
                 </div>
             </div>
         </div>
