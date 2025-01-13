76b921a9c2e5140181a8b7fe3f416c063d6ec73c
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-01-13 14:58:29 +0600

    Signin page error handling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,18 +1,25 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import rb from '../../../public/image/rb.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { FaEyeSlash, FaRegEye } from 'react-icons/fa6'
 const LoginPage = () => {
 
     const router = useRouter()
+    const [errorHandle, setErrorHandle] = useState(false)
+    const [isShowPassword, setIsShowPassword] = useState(false)
+
+    const handlePasswordShow = () => {
+        setIsShowPassword(!isShowPassword)
+    }
+
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors },
     } = useForm()
 
@@ -29,7 +36,10 @@ const LoginPage = () => {
             if (res.status === 200) {
                 router.push("/")
             }
-            console.log(res)
+            if (res.ok === false || res.status === 401) {
+                setErrorHandle(true)
+            }
+            console.log('check login', res)
         } catch (error) {
             console.log(error)
         }
@@ -46,15 +56,32 @@ const LoginPage = () => {
                     <h1 className='text-center text-4xl font-rubik'>Sign In</h1>
                     <div className='flex flex-col gap-2 font-rubik'>
                         <label htmlFor="">Email</label>
-                        <input {...register("email")} className='input input-bordered' type="email" placeholder='Email' />
+                        <input {...register("email", { required: true })} className='input input-bordered' type="email" placeholder='Email' />
+                        {errors.email && <span className='text-red-600'>Email must be required</span>}
                     </div>
                     <div className='flex flex-col gap-2 font-rubik'>
                         <label htmlFor="">Password</label>
-                        <input {...register("password")} className='input input-bordered' type="password" placeholder='Password' />
+                        <div className='relative w-full'>
+                            <input {...register("password", { required: true })} className='input input-bordered w-full' type={isShowPassword ? "text" : "password"} placeholder='Password' />
+                            <div onClick={handlePasswordShow} className='absolute top-[30%] right-3 cursor-pointer'>
+                                {
+                                    isShowPassword ? (
+                                        <FaEyeSlash className='text-xl' />
+                                    ) : (
+                                        <FaRegEye className='text-xl' />
+                                    )
+                                }
+
+
+                            </div>
+                        </div>
+
+                        {errors.email && <span className='text-red-600'>Password must be required</span>}
                     </div>
                     <div className='flex justify-center items-center font-rubik'>
                         <button type='submit' className='btn rounded-none w-32 bg-[#307bc4] text-white'>Sign In</button>
                     </div>
+                    {errorHandle && <span className='text-red-600 font-rubik'>Your email or password doesn't match.</span>}
                     <h1 className='space-x-2'>
                         <span>Create your account!</span>
                         <Link href={"/signup"}>
