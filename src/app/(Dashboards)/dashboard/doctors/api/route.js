615d500547817f3136319fdae3ff8c69e4e6e31a
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-01-18 22:17:54 +0600

    Dashboard admin panel doctor page update

--- src/app/(Dashboards)/dashboard/doctors/api/route.js
+++ src/app/(Dashboards)/dashboard/doctors/api/route.js
@@ -4,8 +4,9 @@ import { NextResponse } from "next/server"
 export const GET = async (request) => {
     try {
         const db = await connectDB()
-        const doctorsCollection = db.collection('doctors')
-        const result = await doctorsCollection.find().toArray()
+        const doctorsCollection = db.collection('users')
+        const query = { role: 'doctor', status: 'approved' }
+        const result = await doctorsCollection.find(query).toArray()
         return NextResponse.json(result)
     } catch (error) {
         return NextResponse.json({ message: "No Data Found", error })
--- src/components/Dashboards/DoctorsDashboard/DoctorsDashboard.jsx
+++ src/components/Dashboards/DoctorsDashboard/DoctorsDashboard.jsx
@@ -45,38 +45,90 @@ const DoctorsDashboard = () => {
                         <FaSearch className='absolute right-2 text-2xl cursor-pointer' />
                     </div>
                 </div>
-                <div className="overflow-x-auto lg:overflow-hidden bg-white">
+                <div className="overflow-x-auto  bg-white">
                     <table className="table font-rubik">
                         {/* head */}
                         <thead>
                             <tr className='text-[15px]'>
-                                <th></th>
-                                <th>ID</th>
-                                <th>Doctor Name</th>
-                                <th>Deparment</th>
-                                <th>Phone</th>
-                                <th>Email</th>
-                                <th></th>
+                                <th>Doctor Info</th>
+                                <th>Specialization</th>
+                                <th>Experience</th>
+                                <th>Available Date</th>
+                                <th>Time and slots</th>
+                                <th>Contact Us</th>
+                                {/* <th>Email</th> */}
                             </tr>
                         </thead>
                         <tbody>
                             {
                                 doctorsData.map(doctor => (
                                     <tr key={doctor._id}>
-                                        <th>
-                                            <Image
-                                                className='w-14 h-14 rounded-full'
-                                                src={doctor?.profilePicture}
-                                                width={500}
-                                                height={300}
-                                                alt='doctor picture'
-                                            />
-                                        </th>
-                                        <td>p-154</td>
-                                        <td>{doctor?.name}</td>
-                                        <td>{doctor?.department}</td>
-                                        <td>{doctor?.contact?.phone}</td>
-                                        <td>{doctor?.contact?.email}</td>
+                                        <td>
+                                            <div className='flex items-center gap-3'>
+                                                <div>
+                                                    {
+                                                        doctor?.image ? (
+                                                            <Image
+                                                                className='w-14 h-14 rounded-full'
+                                                                src={doctor?.image}
+                                                                width={500}
+                                                                height={300}
+                                                                alt='doctor picture'
+                                                            />
+                                                        ) : (
+                                                            <Image
+                                                                className='w-14 h-14 rounded-full'
+                                                                src={`https://i.ibb.co.com/WcTWxsN/nav-img.png`}
+                                                                width={500}
+                                                                height={300}
+                                                                alt='doctor picture'
+                                                            />
+                                                        )
+                                                    }
+                                                </div>
+                                                <div>
+                                                    <p>{doctor?.name || "N/A"}</p>
+                                                    <p>{doctor?.patiend_id || "N/A"}</p>
+                                                </div>
+                                            </div>
+
+                                        </td>
+                                        <td>{doctor?.professional_information?.specialization || "N/A"}</td>
+                                        <td>{doctor?.professional_information?.experience || "N/A"} Year</td>
+                                        <td>
+                                            {
+                                                doctor?.service_details?.available_date ? (
+                                                    <div className='space-y-2'>
+                                                        {
+                                                            doctor?.service_details?.available_date?.map((date, index) => (
+                                                                <p className='' key={index}>{new Date(date).toLocaleDateString()}</p>
+                                                            ))
+                                                        }
+                                                    </div>
+                                                ) : (
+                                                    <p>N/A</p>
+                                                )
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                doctor?.service_details?.time_and_slots[0] ? (
+                                                    <div className='flex items-center gap-2'>
+                                                        <p>{doctor?.service_details?.time_and_slots[0].start}</p>
+                                                        <span>-</span>
+                                                        <p>{doctor?.service_details?.time_and_slots[0].end}</p>
+                                                    </div>
+                                                ) : (
+                                                    <p>N/A</p>
+                                                )
+                                            }
+                                        </td>
+                                        <td>
+                                            <div>
+                                                <p>{doctor?.email}</p>
+                                                <p>{doctor?.phone_number}</p>
+                                            </div>
+                                        </td>
                                         <td>
                                             <div className="dropdown dropdown-end">
                                                 <div tabIndex={0} role="button" className=" m-1">
